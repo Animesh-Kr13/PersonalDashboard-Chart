6a7bb8967a8263f864f32716ae1acd5967dca363
Animesh-Kr13 <kumar.animesh2759@gmail.com>
2021-07-29 17:11:28 +0530

    Responsive Layout and graph functionality

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+// fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+//     .then(res => res.json())
+//     .then(data => {
+//         document.body.style.backgroundImage = `url(${data.urls.regular})`
+// 		document.getElementById("author").textContent = `By: ${data.user.name}`
+//     })
+//     .catch(err => {
+//         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+// )`
+// 		document.getElementById("author").textContent = `By: Dodi Achmad`
+//     })
 
 
 
@@ -132,7 +132,7 @@ async function ChartIt3(){
                 legend: {
                     labels: {
                         font: {
-                            size: 20
+                            size: 18
                         }
                     }
                 }
@@ -168,7 +168,7 @@ async function getData() {
     
     document.getElementById("crypto-top").innerHTML = `
             <div class="bitcoin-name">
-                <img src=${result.image.small} />
+                <img src=${result.image.small} class="bitcoin-image"/>
                 <p>${result.name}</p>
             </div>
         ` + document.getElementById("crypto-top").innerHTML;
@@ -198,7 +198,7 @@ async function getData2() {
     
     document.getElementById("crypto-top-1").innerHTML = `
             <div class="bitcoin-name">
-                <img src=${result.image.small} />
+                <img src=${result.image.small} class="bitcoin-image"/>
                 <p>${result.name}</p>
             </div>
         ` + document.getElementById("crypto-top-1").innerHTML;
@@ -227,7 +227,7 @@ async function getData3() {
     
     document.getElementById("crypto-top-2").innerHTML = `
             <div class="bitcoin-name">
-                <img src=${result.image.small} />
+                <img src=${result.image.small} class="bitcoin-image"/>
                 <p>${result.name}</p>
             </div>
         ` + document.getElementById("crypto-top-2").innerHTML;
